b9cb9fef8db0af8df1d8042f2d75c202c36bdc55
Salomelcb <salomelbatista@gmail.com>
2025-11-17 20:35:43 +0000

    uja funcionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Controllers/AnunciosController.cs
+++ Controllers/AnunciosController.cs
@@ -13,14 +13,97 @@ namespace AutoHubProjeto.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(
+            string? Categoria,
+            string? Marca,
+            int? AnoMin,
+            int? AnoMax,
+            decimal? PrecoMin,
+            decimal? PrecoMax,
+            string? Combustivel,
+            int? KmMax,
+            string? Caixa,
+            string? Ordenar
+        )
         {
-            var anuncios = _db.Anuncios
+            // Query base
+            var query = _db.Anuncios
                 .Include(a => a.IdVeiculoNavigation)
                 .Include(a => a.AnuncioImagems)
-                .ToList();
+                .AsQueryable();
 
-            return View(anuncios);
+            // -------- FILTROS --------
+            if (!string.IsNullOrEmpty(Categoria))
+                query = query.Where(a => a.IdVeiculoNavigation.Categoria == Categoria);
+
+            if (!string.IsNullOrEmpty(Marca))
+                query = query.Where(a => a.IdVeiculoNavigation.Marca == Marca);
+
+            if (AnoMin.HasValue)
+                query = query.Where(a => a.IdVeiculoNavigation.Ano >= AnoMin.Value);
+
+            if (AnoMax.HasValue)
+                query = query.Where(a => a.IdVeiculoNavigation.Ano <= AnoMax.Value);
+
+            if (PrecoMin.HasValue)
+                query = query.Where(a => a.Preco >= PrecoMin.Value);
+
+            if (PrecoMax.HasValue)
+                query = query.Where(a => a.Preco <= PrecoMax.Value);
+
+            if (!string.IsNullOrEmpty(Combustivel))
+                query = query.Where(a => a.IdVeiculoNavigation.Combustivel == Combustivel);
+
+            if (KmMax.HasValue)
+                query = query.Where(a => a.IdVeiculoNavigation.Quilometragem <= KmMax.Value);
+
+            if (!string.IsNullOrEmpty(Caixa))
+                query = query.Where(a => a.IdVeiculoNavigation.Caixa == Caixa);
+
+            // -------- ORDENAR --------
+            Ordenar ??= "relevancia";
+
+            query = Ordenar switch
+            {
+                "preco-desc" => query.OrderByDescending(a => a.Preco),
+                "preco-asc" => query.OrderBy(a => a.Preco),
+                "ano-desc" => query.OrderByDescending(a => a.IdVeiculoNavigation.Ano),
+                "ano-asc" => query.OrderBy(a => a.IdVeiculoNavigation.Ano),
+                "km-asc" => query.OrderBy(a => a.IdVeiculoNavigation.Quilometragem),
+                "km-desc" => query.OrderByDescending(a => a.IdVeiculoNavigation.Quilometragem),
+                _ => query.OrderBy(a => a.IdAnuncio) // relevância default
+            };
+
+            // -------- VIEWMODEL --------
+            var vm = new FiltroAnunciosViewModel
+            {
+                Resultados = query.ToList(),
+
+                Categoria = Categoria,
+                Marca = Marca,
+                AnoMin = AnoMin,
+                AnoMax = AnoMax,
+                PrecoMin = PrecoMin,
+                PrecoMax = PrecoMax,
+                Combustivel = Combustivel,
+                KmMax = KmMax,
+                Caixa = Caixa,
+                Ordenar = Ordenar,
+
+                Categorias = _db.Veiculos
+                    .Select(v => v.Categoria)
+                    .Where(c => c != null && c != "")
+                    .Distinct()
+                    .ToList(),
+
+                Marcas = _db.Veiculos
+                    .Select(v => v.Marca)
+                    .Where(m => m != null && m != "")
+                    .Distinct()
+                    .ToList()
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ Models/FiltrosAnunciosViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace AutoHubProjeto.Models
+{
+    public class FiltroAnunciosViewModel
+    {
+        // ----- FILTROS -----
+        public string Categoria { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public int? AnoMin { get; set; }
+        public int? AnoMax { get; set; }
+
+        public decimal? PrecoMin { get; set; }
+        public decimal? PrecoMax { get; set; }
+
+        public int? KmMax { get; set; }
+
+
+        public string Combustivel { get; set; }
+        public string Caixa { get; set; }
+        public string Localizacao { get; set; }
+
+        public string Ordenar { get; set; }
+
+        // ----- RESULTADOS -----
+        public List<Anuncio> Resultados { get; set; }
+
+        // ----- LISTAS DINÂMICAS -----
+        public List<string> Marcas { get; set; }
+        public List<string> Categorias { get; set; }
+        public List<string> Localizacoes { get; set; }
+    }
+}

f0bba43fb6c6b369427b68ed00ecfde4a8deb061
Salomelcb <salomelbatista@gmail.com>
2025-11-24 10:33:24 +0000

    ta mais bonito

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string Email, string Password)
+    {
+        // Depois ligamos à BD
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost]
+    public IActionResult Register()
+    {
+        // Depois ligamos à BD
+        return RedirectToAction("Preferencias");
+    }
+
+    public IActionResult Preferencias()
+    {
+        return View();
+    }
+}
